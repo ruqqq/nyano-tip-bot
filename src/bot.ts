636237899eed7e0ec7b64f8b8a2befbd4e6e7035
Faruq Rasid <me@ruqqq.sg>
2021-11-30 07:46:50 +0800

    Add wrapNext helper

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,30 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { Bot } from "grammy";
+import { Bot, NextFunction } from "grammy";
 import { MnanoContext } from "./context";
 import { BotService } from "./bot-service";
+
+function wrapNext(fn: (ctx: MnanoContext) => Promise<void>): (ctx: MnanoContext, next: NextFunction) => Promise<void> {
+  return async (ctx, next) => {
+    await fn(ctx);
+    next();
+  };
+}
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const bot = new Bot<MnanoContext>(process.env.BOT_TOKEN!);
 
-bot.on("message", BotService.handleMessage);
+bot.command("start", (ctx, next) => {
+  if (!ctx.update.message) {
+    next();
+    return;
+  }
+
+  const payload = ctx.update.message?.text.replace("/start", "").trim();
+  ctx.reply(`Placeholder. Payload: ${payload}`);
+  next();
+});
+
+bot.on("message", wrapNext(BotService.handleMessage));
 
 bot.start();
